f885d0a66722a0f6a26e29103e4865fd92f9c212
werasak <jokerwwe0542@gmail.com>
2022-12-13 17:37:19 +0700

    "lab7_4"

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,21 +3,26 @@
 using namespace std;
 //การเติมคำตอบในช่องนี้ ขอให้เติมเฉพาะ Function adiff() เท่านั้น โดยห้ามเติม Function main() มาในนี้
 
-int adiff(int x,int y){
-    if(x>y){
-        return (x-y)%360;
+int adiff(int A,int B){
+    int z;
+    if(A>B){
+        z=(A-B)%360;
     }else{
-        return (y-x)%360;
+        z=(B-A)%360;
     }
+    if(z > 180){
+        return 360-z;
+    }
+    return z;
 }
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
   cout << adiff(60,244);
 }
